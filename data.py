c8ace385ef464b06b089d76a3f9c5dcbf5c108f3
Daniel <danswe@hotmail.com>
2018-03-21 09:21:55 +0100

    Video 1

---
+++ data.py
@@ -0,0 +1,22 @@
+def Articles():
+    articles = [
+        {'id': 1,
+         'title': 'Article One',
+         'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+         'author': 'Daniel Svensson',
+         'create_date': '19-03-2018'
+         },
+        {'id': 2,
+         'title': 'Article One',
+         'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+         'author': 'Sven Svensson',
+         'create_date': '19-03-2018'
+         },
+        {'id': 3,
+         'title': 'Article One',
+         'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+         'author': 'Daniel Svensson',
+         'create_date': '19-03-2018'
+         },
+    ]
+    return articles
